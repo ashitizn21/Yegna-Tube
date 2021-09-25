c97579ab15fec8298f1edf47c3ac4ed6389474ad
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2021-09-25 10:12:16 +0300

    first commit

---
+++ include/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    $server_host = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "YegnaTube";
+
+    $con = new PDO("mysql:host=$server_host;dbname=$db_name", $username, $password);
+
+    
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ዬኛ Tube</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/images/photo_2021-07-27_01-09-48.jpg" >
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div id="pageContent">
+        <div id="mastHeadContainer">
+            <button class="navShowHide">click here</button>
+        asdfghjkl 
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+            <div id="mainContentContanier">
+                Hello Yegna tube
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
